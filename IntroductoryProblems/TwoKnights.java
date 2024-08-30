dcba1b2edb9cfc089ed4f88fa2ae8c0bdcce269c
pranay-4696 <pranaymacharla18@gmail.com>
2024-08-31 02:21:14 +0530

    Two sets and knights

---
+++ IntroductoryProblems/TwoKnights.java
@@ -0,0 +1,5 @@
+public class TwoKnights {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ IntroductoryProblems/TwoSets.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class TwoSets {
+    static BufferedReader br;
+    static BufferedWriter bw;
+    public static void main(String[] args) throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        solve(n);
+    }
+
+    private static void solve(int n) {
+
+    }
+}
