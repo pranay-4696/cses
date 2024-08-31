2f53dba237da2aaaf49e90d4848b8d4b39596cd0
pranay-4696 <pranaymacharla18@gmail.com>
2024-08-31 16:57:59 +0530

    Introductory Problems

--- IntroductoryProblems/TwoKnights.java
+++ IntroductoryProblems/TwoKnights.java
@@ -1,5 +1,15 @@
+import java.io.*;
+
 public class TwoKnights {
-    public static void main(String[] args) {
-        
+    static BufferedReader br;
+    static BufferedWriter bw;
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 1; i <= n; i++) {
+            long tot = (i*i)*((i*i)-1);
+            tot /= 2;
+        }
     }
 }
--- IntroductoryProblems/TwoSets.java
+++ IntroductoryProblems/TwoSets.java
@@ -1,4 +1,7 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TwoSets {
     static BufferedReader br;
     static BufferedWriter bw;
@@ -7,12 +10,35 @@ public class TwoSets {
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         solve(n);
+        bw.flush();
     }
 
     private static void solve(int n) throws Exception {
-        long sum = n*(n+1)/2;
-        if(n % 4 != 0) bw.write("NO SOLUTION\n");
-        //Solution exists
+        long sum = (long)n*(n+1)/2;
+        if(sum % 2 != 0) {
+            bw.write("NO\n");
+            return;
+        }
+        bw.write("YES\n");
+        List<Integer> ls1 = new ArrayList<>();
+        List<Integer> ls2 = new ArrayList<>();
+        long k = sum/2;
+        int i = 0;
+        for(i = n; i >= 1 && k - i >= 0; i--) {
+            ls1.add(i);
+            k -= i;
+        }
+        if(k != 0)
+            ls1.add((int)k);
+        for(int j = 1; j <= i; j++) {
+            if(j == k) continue;
+            ls2.add(j);
+        }
+        bw.write(ls1.size()+"\n");
+        for(int x : ls1)  bw.write(x+" ");
+        bw.write("\n");
+        bw.write(ls2.size()+"\n");
+        for(int x : ls2)  bw.write(x+" ");
         return;
     }
 }
